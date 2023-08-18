c002359ac86840ed513359b1b9d4ff3c5f7517fd
Isaac <zyskson@gmail.com>
2023-08-18 11:46:21 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       
       <body className={inter.className}>
-   <ThemeProvider attribute="class" defaultTheme="system" enableSystem>  
+   <ThemeProvider attribute="class" defaultTheme="light">  
           <LayoutWrapper>
         {children}
         </LayoutWrapper>
